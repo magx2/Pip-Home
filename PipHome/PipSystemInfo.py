9c1eace21f8707c2ec9fac20923eccab6af9088e
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-03-29 19:01:37 +0200

    Make code look nicer

--- PipHome/PipSystemInfo.py
+++ PipHome/PipSystemInfo.py
@@ -23,78 +23,62 @@ class PipSystemInfo:
             return "N/A"
 
     def start(self):
-        self._scheduler.add_task("ip check", self._load_ip, group="PipSystemInfo")
-        self._scheduler.add_task("CPU %", self._load_cpu_percent, group="PipSystemInfo")
-        self._scheduler.add_task("CPU logical cores", self._load_cpu_cores, group="PipSystemInfo")
-        self._scheduler.add_task("CPU load avg", self._load_cpu_load_avg, group="PipSystemInfo")
-        self._scheduler.add_task("Memory info", self._load_mem, group="PipSystemInfo")
-        self._scheduler.add_task("Disk info", self._load_disk, group="PipSystemInfo")
+        self._schedule_task("ip check", self._load_ip, HOUR)
+        self._schedule_task("CPU %", self._load_cpu_percent, SECOND * 10)
+        self._schedule_task("CPU logical cores", self._load_cpu_cores, ONCE)
+        self._schedule_task("CPU load avg", self._load_cpu_load_avg, SECOND * 10)
+        self._schedule_task("Memory info", self._load_mem, SECOND * 10)
+        self._schedule_task("Disk info", self._load_disk, SECOND * 10)
 
     def stop(self):
         self._scheduler.remove_tasks_with_group("PipSystemInfo")
 
-    def _load_ip(self):
+    def _schedule_task(self, task_name, function, time):
+        self._scheduler.add_task(task_name, self._build_task(task_name, function, time), group="PipSystemInfo")
+
+    def _build_task(self, task_name, function, time):
         try:
-            self._logger.debug("Checking IP address")
-            ip = get('https://api.ipify.org').text
-            now = datetime.now().strftime("%H:%M:%S")
-            self._system_info["connection"] = f"Active ({now})"
-            self._system_info["ip"] = ip
+            function()
         except Exception as e:
-            self._logger.error(f"Cannot get IP! {e}")
-            self._system_info["connection"]("Unavailable")
-            self._system_info["ip"]("N/A")
-        return HOUR
+            self._logger.error(f"Task `{task_name}` failed with exception {e}")
+        return time
+
+    def _load_ip(self):
+        self._logger.debug("Checking IP address")
+        ip = get('https://api.ipify.org').text
+        now = datetime.now().strftime("%H:%M:%S")
+        self._system_info["connection"] = f"Active ({now})"
+        self._system_info["ip"] = ip
 
     def _load_cpu_percent(self):
-        try:
-            self._logger.debug("Checking CPU percent")
-            cpu_percent = round(psutil.cpu_percent(), 2)
-            self._system_info["cpu_percent"] = str(cpu_percent) + "%"
-        except Exception as e:
-            self._logger.error(f"Cannot check CPU percent! {e}")
-        return SECOND * 10
+        self._logger.debug("Checking CPU percent")
+        cpu_percent = round(psutil.cpu_percent(), 2)
+        self._system_info["cpu_percent"] = str(cpu_percent) + "%"
 
     def _load_cpu_cores(self):
-        try:
-            self._logger.debug("Checking CPU cores")
-            cpu_cores = round(psutil.cpu_count(), 2)
-            self._system_info["cpu_cores"] = str(cpu_cores)
-        except Exception as e:
-            self._logger.error(f"Cannot check CPU cores! {e}")
-        return ONCE
+        self._logger.debug("Checking CPU cores")
+        cpu_cores = round(psutil.cpu_count(), 2)
+        self._system_info["cpu_cores"] = str(cpu_cores)
 
     def _load_cpu_load_avg(self):
-        try:
-            self._logger.debug("Checking CPU load avg")
-            cpu_load_avg = ", ".join(
-                map(str,
-                    map(lambda x: round(x, 2),
-                        psutil.getloadavg())))
-            self._system_info["cpu_load_avg"] = str(cpu_load_avg)
-        except Exception as e:
-            self._logger.error(f"Cannot check CPU load avg! {e}")
-        return SECOND * 10
+        self._logger.debug("Checking CPU load avg")
+        cpu_load_avg = ", ".join(
+            map(str,
+                map(lambda x: round(x, 2),
+                    psutil.getloadavg())))
+        self._system_info["cpu_load_avg"] = str(cpu_load_avg)
 
     def _load_mem(self):
-        try:
-            self._logger.debug("Checking Memory")
-            memory = psutil.virtual_memory()
-            self._system_info["mem_total"] = format_bytes(memory.total)
-            self._system_info["mem_used"] = format_bytes(memory.used)
-            self._system_info["mem_free"] = format_bytes(memory.total - memory.used)
-        except Exception as e:
-            self._logger.error(f"Cannot check memory! {e}")
-        return SECOND * 10
+        self._logger.debug("Checking Memory")
+        memory = psutil.virtual_memory()
+        self._system_info["mem_total"] = format_bytes(memory.total)
+        self._system_info["mem_used"] = format_bytes(memory.used)
+        self._system_info["mem_free"] = format_bytes(memory.total - memory.used)
 
     def _load_disk(self):
-        try:
-            path = self._config["system.disk_check_path"]
-            self._logger.debug(f"Checking Disk for path `{path}`")
-            disk = psutil.disk_usage(path)
-            self._system_info["disk_total"] = format_bytes(disk.total)
-            self._system_info["disk_used"] = format_bytes(disk.used)
-            self._system_info["disk_free"] = format_bytes(disk.free)
-        except Exception as e:
-            self._logger.error(f"Cannot check disk! {e}")
-        return SECOND * 10
+        path = self._config["system.disk_check_path"]
+        self._logger.debug(f"Checking Disk for path `{path}`")
+        disk = psutil.disk_usage(path)
+        self._system_info["disk_total"] = format_bytes(disk.total)
+        self._system_info["disk_used"] = format_bytes(disk.used)
+        self._system_info["disk_free"] = format_bytes(disk.free)
