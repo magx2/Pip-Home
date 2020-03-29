1bb3eeae4236fbca6887a215a9042224154ddc8f
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-03-29 23:51:00 +0200

    Add `Home` component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ClockComponent} from "./clock/clock.component";
 import {MiscComponent} from "./misc/misc.component";
+import {HomeComponent} from "./home/home.component";
 
 
 const routes: Routes = [
   {path: "misc", component: MiscComponent},
+  {path: "home", component: HomeComponent},
   {path: "", component: ClockComponent}
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ClockComponent} from './clock/clock.component';
 import {MiscComponent} from './misc/misc.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClockComponent,
-    MiscComponent
+    MiscComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
