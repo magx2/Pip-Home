72abc265fd6029ff884ff40c68e2bea709eee4b5
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-03-29 23:49:04 +0200

    Implement clock

--- client/src/app/clock/clock.component.ts
+++ client/src/app/clock/clock.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-clock',
@@ -6,11 +7,28 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./clock.component.sass']
 })
 export class ClockComponent implements OnInit {
+  hour: string = "&nbsp;&nbsp;";
+  minute: string = "&nbsp;&nbsp;";
+  separator: string = "&nbsp;";
+  date: string = "";
+  timer = interval(1000);
+  subscription: Subscription;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.subscription = this.timer.subscribe(_ => {
+      const date = new Date();
+      this.hour = date.getHours().toString();
+      this.minute = date.getMinutes().toString();
+      this.separator = this.separator === ":" ? "&nbsp;" : ":";
+      const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+      this.date = date.toLocaleDateString("en-US", options)
+    })
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
