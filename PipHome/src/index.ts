74fd3fc56f6ed646819abfb5b19364bf4d04f146
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-04-01 22:52:07 +0200

    Fit look & feel

--- PipHome/src/index.ts
+++ PipHome/src/index.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from 'electron';
+import {app, BrowserWindow} from 'electron';
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -9,7 +10,7 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
+    height: 480,
     width: 800,
   });
 
